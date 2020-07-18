225fbd8e59f9126b67b1da3d2b97abbf7f7af5da
rajanmagar <rajanmagarrr@gmail.com>
2020-07-18 17:50:56 +0530

    setup public route with react router dom :fire:

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
-}
+  return (
+    <Router>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+    </Router>
+  );
+};
 
 export default App;
