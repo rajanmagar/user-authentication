25b5b91d50e7b4c022e52b343aac62121a148369
Rajan Magar <rajanmagarrr@gmail.com>
2020-07-19 05:30:36 +0530

    firebase setup :fire:

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
 
 import * as ROUTES from '../../constants/routes';
 
@@ -14,6 +15,7 @@ const App = () => {
       <hr />
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
     </Router>
   );
 };
